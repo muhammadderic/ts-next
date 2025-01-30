2d25a068117811bcec362974be73272cb7afc224
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-30 14:57:11 +0700

    Create startup query, get all startups from sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/queries";
+import { Startup } from "@/sanity/types";
 
-export default function Home() {
+export default async function Home() {
+  // OUTPUT: startups = a list [] of startups {}
+  const startups = await client.fetch(STARTUPS_QUERY);
+
   return (
     <div>
       <h1>Hello Deric</h1>
       <Link href="/add">Add</Link>
+
+      <ul>
+        {startups.map((startup: Startup) => (
+          <li key={startup._id}>
+            <h5>{startup.title}</h5>
+            <p>{startup.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
